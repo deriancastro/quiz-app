2873ae298f7b22290a4985173250bd769075cfdf
Derian Castro <deriancastro@hotmail.com>
2021-04-30 18:22:05 +0200

    find Error continue...

--- src/setupBookmarks.js
+++ src/setupBookmarks.js
@@ -1,8 +1,13 @@
 import getElement from './utils/getElement'
+import getAllElements from './utils/getAllElements'
 
 export default function setupBookmarks() {
-  const bookmarkSelector = getElement('.js-quiz-card-bookmark')
-  bookmarkSelector.addEventListener('click', () => {
-    bookmarkSelector.classList.toggle('quiz-card__bookmark--selected')
+  const cards = getAllElements('.quiz-card')
+
+  cards.forEach(card => {
+    const quizCardMarkbook = getElement('.js-quiz-card-bookmark', card)
+    quizCardMarkbook.addEventListener('click', () => {
+      quizCardMarkbook.classList.toggle('quiz-card__bookmark--selected')
+    })
   })
 }
