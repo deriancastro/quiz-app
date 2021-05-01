8e34fdd09b844e016d9bcc2ba385b8f24583c4b9
Derian Castro <deriancastro@hotmail.com>
2021-05-01 19:36:45 +0200

    Add setupMenuBar

---
+++ src/setupMenuBar.js
@@ -0,0 +1,6 @@
+import getElement from './utils/getElement'
+
+export default function setupMenuBar() {
+  const menuButton = getElement('.js-menu-button')
+  const modusButton = getElement('.js-modus-button')
+}
